fa79b4f1b2ae527d4bd3df89ff260b9af377d4ab
Lokendra <lokilokendra12345@gmail.com>
2020-09-28 18:05:03 +0530

    [lokendra]Refactor.code added for master

---
+++ LineComparisionProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparisionProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the line comparision computation");
+        }
+    }
+}
