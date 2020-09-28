3f58fabf8cd3c4bda6c03c8cc6b2f69559fcdcd6
Lokendra <lokilokendra12345@gmail.com>
2020-09-28 18:11:03 +0530

    [lokendra]Refactor.added a code for usercase1

--- LineComparisionProblem/Program.cs
+++ LineComparisionProblem/Program.cs
@@ -6,7 +6,22 @@ namespace LineComparisionProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the line comparision computation");
+            double[] x = new double[2];
+            double[] y = new double[2];
+            Random random = new Random();
+
+            for (int index = 0; index < 2; index++)
+            {
+                x[index] = random.Next(-100, 100);
+                y[index] = random.Next(-100, 100);
+            }
+            Console.WriteLine("Coordinates are ({0}, {1}) & ({2}, {3})", x[0], y[0], x[1], y[1]);
+
+            double length = Math.Sqrt(Math.Pow((x[1] - x[0]), 2) + Math.Pow((y[1] - y[0]), 2));
+            Console.WriteLine("Length of the line : " + Math.Round(length, 4));
+
+
+
         }
     }
 }
