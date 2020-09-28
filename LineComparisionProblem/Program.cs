f205657e00e9bc6bed2fe5f05f8d1b83e01d96a7
Lokendra <lokilokendra12345@gmail.com>
2020-09-28 18:19:15 +0530

    [lokendra]Refactor.merged usercase2

--- LineComparisionProblem/Program.cs
+++ LineComparisionProblem/Program.cs
@@ -8,18 +8,30 @@ namespace LineComparisionProblem
         {
             double[] x = new double[2];
             double[] y = new double[2];
+            double[] length = new double[2];
+            int lineCount = 0;
+
             Random random = new Random();
 
-            for (int index = 0; index < 2; index++)
+            while (lineCount < 2)
             {
-                x[index] = random.Next(-100, 100);
-                y[index] = random.Next(-100, 100);
+                for (int index = 0; index < 2; index++)
+                {
+                    x[index] = random.Next(-100, 100);
+                    y[index] = random.Next(-100, 100);
+                }
+                Console.WriteLine("Coordinates of line {0} are ({1}, {2}) & ({3}, {4})", lineCount + 1, x[0], y[0], x[1], y[1]);
+
+                length[lineCount] = Math.Sqrt(Math.Pow((x[1] - x[0]), 2) + Math.Pow((y[1] - y[0]), 2));
+                Console.WriteLine("Length of the line {0}: {1}", lineCount + 1, Math.Round(length[lineCount], 4));
+
+                lineCount++;
             }
-            Console.WriteLine("Coordinates are ({0}, {1}) & ({2}, {3})", x[0], y[0], x[1], y[1]);
 
-            double length = Math.Sqrt(Math.Pow((x[1] - x[0]), 2) + Math.Pow((y[1] - y[0]), 2));
-            Console.WriteLine("Length of the line : " + Math.Round(length, 4));
-
+            if (length[0].Equals(length[1]))
+                Console.WriteLine("Both lines are equal");
+            else
+                Console.WriteLine("Lines are not equal");
 
 
         }
